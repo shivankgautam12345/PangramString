a5152c7b91f913e532d31d55cfc20f06ce2b3fd7
shivankgautam12345 <142514350+shivankgautam12345@users.noreply.github.com>
2023-10-18 11:32:28 +0530

    Add files via upload

---
+++ PangramApp.java
@@ -0,0 +1,17 @@
+package com.cloudvandana.solutions;
+
+
+import java.util.Scanner;
+
+public class PangramApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		Pangram p=new Pangram();
+		p.checksPangram(str);
+		
+	}
+
+}
